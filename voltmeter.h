d49268de95c5aad1def3425abd4d8867af24f130
enjoyneering <enjoyneering79@gmail.com>
2017-09-17 17:59:35 -0400

    Update voltmeter.h

--- voltmeter.h
+++ voltmeter.h
@@ -1,6 +1,6 @@
-/******************************************************************************/
+/***************************************************************************************************/
 /*
-  This is an Arduino "Voltmeter" module for "Bench Power Supply" project.
+  This is an Arduino "voltmeter" module for "Bench Power Supply" project.
 
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
@@ -9,7 +9,7 @@
 
   BSD license, all text above must be included in any redistribution
 */
-/******************************************************************************/
+/***************************************************************************************************/
 #ifndef voltmeter_h
 #define voltmeter_h
 
@@ -20,13 +20,13 @@
 #endif
  #include "adc.h"
 
-#define R1_DIVIDER                  100.50                                 //R1 voltage divider, kOhm. min input resistance is 30v/0.001a > 30kOhm 
-#define R2_DIVIDER                  8.21                                   //R2 voltage divider, kOhm. ADC optimized for input impedance  < 10kOhm
-#define VOLTAGE_DIVIDER             R2_DIVIDER / (R1_DIVIDER + R2_DIVIDER) //voltage divider coef.
+#define R1_DIVIDER                  100.50                                 //R1 voltage divider in kOhm, min input resistance is 30v/0.001a > 30kOhm 
+#define R2_DIVIDER                  8.21                                   //R2 voltage divider in kOhm, ADC optimized for input impedance  < 10kOhm
+#define VOLTAGE_DIVIDER             R2_DIVIDER / (R1_DIVIDER + R2_DIVIDER) //voltage divider coeff.
 
 #define VOLTMETER_PIN               A0                                     //voltmeter pin
-#define VOLTMETER_ERROR             0.021                                  //to show "0.00",  if connected to ground
-#define OVERSAMPLED_VOLTMETER_ERROR 0.001                                  //to show "0.000", if connected to ground
+#define VOLTMETER_ERROR             0.021                                  //for future use
+#define OVERSAMPLED_VOLTMETER_ERROR 0.001                                  //for future use
 
 float readVoltage(uint8_t adc_pin_number = VOLTMETER_PIN, float voltage_divider = VOLTAGE_DIVIDER);
 float readOversamplingVoltage(uint8_t adc_pin_number = VOLTMETER_PIN, float voltage_divider = VOLTAGE_DIVIDER);
